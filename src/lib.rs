5c9a0a26fc3dd9952495c544dda4504f03132c01
Nathan Fox <fuchsnj@gmail.com>
2023-04-28 17:07:14 -0400

    save

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,13 @@
 #![deny(unused_comparisons)]
 #![allow(clippy::module_name_repetitions)]
 
-pub mod prelude;
-pub use compiler::expression::query;
-pub use compiler::{
-    compile, compile_with_external, compile_with_state, function,
-    runtime::{Runtime, RuntimeResult, Terminate},
-    state, value, CompilationResult, CompileConfig, Compiler, Context, Expression, Function,
-    Program, ProgramInfo, SecretTarget, Target, TargetValue, TargetValueRef, VrlRuntime,
-};
-pub use diagnostic;
-pub use vrl_core::TimeZone;
+// pub mod prelude;
+// pub use compiler::expression::query;
+// pub use compiler::{
+//     compile, compile_with_external, compile_with_state, function,
+//     runtime::{Runtime, RuntimeResult, Terminate},
+//     state, value, CompilationResult, CompileConfig, Compiler, Context, Expression, Function,
+//     Program, ProgramInfo, SecretTarget, Target, TargetValue, TargetValueRef, VrlRuntime,
+// };
+// pub use diagnostic;
+// pub use vrl_core::TimeZone;
