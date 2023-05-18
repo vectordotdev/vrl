dd239f05477cfc4ab89ee36d47dcb3a871a6e9fd
m.nabokikh <maksim.nabokikh@flant.com>
2023-05-18 11:35:17 +0200

    Parse timezone to the native VRL timezone format
    Signed-off-by: m.nabokikh <maksim.nabokikh@flant.com>

--- src/stdlib/format_timestamp.rs
+++ src/stdlib/format_timestamp.rs
@@ -1,20 +1,29 @@
 use crate::compiler::prelude::*;
+use crate::compiler::TimeZone;
 use chrono::{
     format::{strftime::StrftimeItems, Item},
-    DateTime, TimeZone, Utc,
+    DateTime, Local, Utc,
 };
-use chrono_tz::Tz;
+use std::borrow::Borrow;
 
-fn format_timestamp_with_tz<Tz2: TimeZone>(bytes: Value, ts: Value, tz: &Tz2) -> Resolved
-where
-    Tz2::Offset: fmt::Display,
-{
-    let bytes = bytes.try_bytes()?;
-    let format = String::from_utf8_lossy(&bytes);
-    let ts = ts.try_timestamp()?;
-    let ts = ts.with_timezone(tz);
+fn format_timestamp_with_tz(ts: Value, format: Value, timezone: Option<Value>) -> Resolved {
+    let format = format.try_bytes()?;
+    let format = String::from_utf8_lossy(&format);
+    let items = StrftimeItems::new(&format)
+        .map(|item| match item {
+            Item::Error => Err("invalid format".into()),
+            _ => Ok(item),
+        })
+        .collect::<ExpressionResult<Vec<_>>>()?;
 
-    try_format_tz(&ts, &format).map(Into::into)
+    let ts: DateTime<Utc> = ts.try_timestamp()?;
+
+    let ts = match timezone {
+        None => ts.format_with_items(items.into_iter()).to_string(),
+        Some(timezone) => try_format_with_timezone(ts, items, timezone)?,
+    };
+
+    Ok(ts).map(Into::into)
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -38,7 +47,7 @@ impl Function for FormatTimestamp {
                 required: true,
             },
             Parameter {
-                keyword: "tz",
+                keyword: "timezone",
                 kind: kind::BYTES,
                 required: false,
             },
@@ -53,9 +62,14 @@ impl Function for FormatTimestamp {
     ) -> Compiled {
         let value = arguments.required("value");
         let format = arguments.required("format");
-        let tz: Option<Box<dyn Expression>> = arguments.optional("tz");
+        let timezone: Option<Box<dyn Expression>> = arguments.optional("timezone");
 
-        Ok(FormatTimestampFn { value, format, tz }.as_expr())
+        Ok(FormatTimestampFn {
+            value,
+            format,
+            timezone,
+        }
+        .as_expr())
     }
 
     fn examples(&self) -> &'static [Example] {
@@ -67,7 +81,7 @@ impl Function for FormatTimestamp {
             },
             Example {
                 title: "format timestamp with tz",
-                source: r#"format_timestamp!(t'2021-02-10T23:32:00+00:00', format: "%d %B %Y %H:%M", tz: "Europe/Berlin")"#,
+                source: r#"format_timestamp!(t'2021-02-10T23:32:00+00:00', format: "%d %B %Y %H:%M", timezone: "Europe/Berlin")"#,
                 result: Ok("11 February 2021 00:32"),
             },
         ]
@@ -78,7 +92,7 @@ impl Function for FormatTimestamp {
 struct FormatTimestampFn {
     value: Box<dyn Expression>,
     format: Box<dyn Expression>,
-    tz: Option<Box<dyn Expression>>,
+    timezone: Option<Box<dyn Expression>>,
 }
 
 impl FunctionExpression for FormatTimestampFn {
@@ -86,24 +100,12 @@ impl FunctionExpression for FormatTimestampFn {
         let bytes = self.format.resolve(ctx)?;
         let ts = self.value.resolve(ctx)?;
 
-        match self.tz.clone() {
-            None => format_timestamp_with_tz(bytes, ts, &Utc),
+        match self.timezone.clone() {
             Some(tz) => {
-                let tz = &tz.resolve(ctx)?.try_bytes()?;
-                let tz = String::from_utf8_lossy(tz);
-                let tz: Tz = match tz {
-                    std::borrow::Cow::Borrowed("Local") => {
-                        // Get the local remap timezone (Local by default, but with the possibility to be overridden from the config)
-                        String::from(*ctx.timezone())
-                            .parse()
-                            .map_err(|err| format!("unable to parse timezone: {err}"))?
-                    }
-                    _ => tz
-                        .parse()
-                        .map_err(|err| format!("unable to parse timezone: {err}"))?,
-                };
-                format_timestamp_with_tz(bytes, ts, &tz)
+                let tz = tz.resolve(ctx)?;
+                format_timestamp_with_tz(ts, bytes, Some(tz))
             }
+            None => format_timestamp_with_tz(ts, bytes, None),
         }
     }
 
@@ -112,18 +114,28 @@ impl FunctionExpression for FormatTimestampFn {
     }
 }
 
-fn try_format_tz<Tz2: TimeZone>(dt: &DateTime<Tz2>, format: &str) -> ExpressionResult<String>
-where
-    Tz2::Offset: fmt::Display,
-{
-    let items = StrftimeItems::new(format)
-        .map(|item| match item {
-            Item::Error => Err("invalid format".into()),
-            _ => Ok(item),
-        })
-        .collect::<ExpressionResult<Vec<_>>>()?;
+fn try_format_with_timezone(
+    ts: DateTime<Utc>,
+    items: Vec<Item>,
+    timezone: Value,
+) -> ExpressionResult<String> {
+    let timezone = timezone.try_bytes()?;
+    let timezone = String::from_utf8_lossy(&timezone);
+    let parsed_timezone = TimeZone::parse(timezone.borrow());
 
-    Ok(dt.format_with_items(items.into_iter()).to_string())
+    match parsed_timezone {
+        Some(parsed_timezone) => match parsed_timezone {
+            TimeZone::Local => Ok(ts
+                .with_timezone(&Local)
+                .format_with_items(items.into_iter())
+                .to_string()),
+            TimeZone::Named(tz) => Ok(ts
+                .with_timezone(&tz)
+                .format_with_items(items.into_iter())
+                .to_string()),
+        },
+        None => Err(format!("unable to parse timezone: {timezone}").into()),
+    }
 }
 
 #[cfg(test)]
@@ -159,7 +171,7 @@ mod tests {
         tz {
             args: func_args![value: Utc.timestamp_opt(10, 0).single().expect("invalid timestamp"),
                              format: "%+",
-                             tz: "Europe/Berlin"],
+                             timezone: "Europe/Berlin"],
             want: Ok(value!("1970-01-01T01:00:10+01:00")),
             tdef: TypeDef::bytes().fallible(),
         }
@@ -167,8 +179,16 @@ mod tests {
         tz_local {
             args: func_args![value: Utc.timestamp_opt(10, 0).single().expect("invalid timestamp"),
                              format: "%s",
-                             tz: "Local"],
-            want: Ok(value!("10")), // Check that there is no error for the Local timezone
+                             timezone: "local"],
+            want: Ok(value!("10")), // Check that there is no error for the local timezone
+            tdef: TypeDef::bytes().fallible(),
+        }
+
+        invalid_tz {
+            args: func_args![value: Utc.timestamp_opt(10, 0).single().expect("invalid timestamp"),
+                             format: "%+",
+                             timezone: "llocal"],
+            want: Err("unable to parse timezone: llocal"),
             tdef: TypeDef::bytes().fallible(),
         }
     ];
