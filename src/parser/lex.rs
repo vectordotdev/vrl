35ddaa83def72ad282cc8706e0d16005722b4dc8
arthmoeros <artu.saavedra@gmail.com>
2023-06-02 17:05:53 -0400

    Update src/parser/lex.rs as suggested!
    Co-authored-by: Nathan Fox <fuchsnj@gmail.com>

--- src/parser/lex.rs
+++ src/parser/lex.rs
@@ -1308,7 +1308,7 @@ fn unescape_string_literal(mut s: &str) -> String {
                 b'n' => '\n',
                 b'r' => '\r',
                 b't' => '\t',
-                b'\0' => '\0',
+                b'0' => '\0',
                 b'{' => '{',
                 _ => unimplemented!("invalid escape"),
             };
