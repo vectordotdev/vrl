3718da06eb95b4d582b5d6291dea00d8464f1175
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2023-08-18 15:31:36 -0400

    avoid redundant installations

--- scripts/check_features.sh
+++ scripts/check_features.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cargo install cargo-hack --version 0.5.29 --force --locked
+if ! cargo install --list | grep -q "cargo-hack v0.5.29"; then
+    cargo install cargo-hack --version 0.5.29 --force --locked
+fi
 
 echo "Check that all features can compile"
 cargo hack check --feature-powerset --depth 1
