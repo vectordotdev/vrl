d454a2fd0e29bf3e9ca0bb7887087e1d3d9e0483
Thayne McCombs <thayne@lucidchart.com>
2024-01-18 17:16:00 -0700

    enhancement(replace_with): Pass object instead of array to closure
    This allows us to expose the named capture groups with names.

--- src/stdlib/replace_with.rs
+++ src/stdlib/replace_with.rs
@@ -1,8 +1,9 @@
 use std::collections::BTreeMap;
 
-use regex::Regex;
+use regex::{CaptureMatches, CaptureNames, Captures, Regex};
 
 use crate::compiler::prelude::*;
+use crate::value;
 
 fn replace_with<T>(
     value: Value,
@@ -22,13 +23,21 @@ where
         _ => return Ok(value),
     };
     let captures = pattern.captures_iter(&haystack);
-    make_replacement(captures, &haystack, count, ctx, runner)
+    make_replacement(
+        captures,
+        &haystack,
+        count,
+        pattern.capture_names(),
+        ctx,
+        runner,
+    )
 }
 
 fn make_replacement<T>(
-    caps: regex::CaptureMatches,
+    caps: CaptureMatches,
     haystack: &str,
     count: usize,
+    capture_names: CaptureNames,
     ctx: &mut Context,
     runner: closure::Runner<T>,
 ) -> Resolved
@@ -45,7 +54,7 @@ where
         // Safe to unrap because the 0th index always includes the full match.
         let m = captures.get(0).unwrap(); // full match
 
-        let mut value = captures_to_value(&captures);
+        let mut value = captures_to_value(&captures, capture_names.clone());
         runner.map_value(ctx, &mut value)?;
         let replacement = value.try_bytes_utf8_lossy()?;
 
@@ -61,15 +70,31 @@ where
     Ok(replaced.into())
 }
 
-fn captures_to_value(captures: &regex::Captures) -> Value {
-    // return an array of the capture groups
-    captures
-        .iter()
-        .map(|m| match m {
-            Some(m) => m.as_str().into(),
-            None => Value::Null,
-        })
-        .collect()
+fn captures_to_value(captures: &Captures, capture_names: CaptureNames) -> Value {
+    let full_match: Value = captures.get(0).unwrap().as_str().into();
+    // The length includes the total match, so subtract 1
+    let mut capture_groups: Vec<Value> = Vec::with_capacity(captures.len() - 1);
+    let mut named_groups: ObjectMap = BTreeMap::new();
+
+    // We skip the first entry, because it is for the full match, which we have already
+    // extracted
+    for (idx, name) in capture_names.enumerate().skip(1) {
+        let value: Value = if let Some(group) = captures.get(idx) {
+            group.as_str().into()
+        } else {
+            Value::Null
+        };
+        if let Some(name) = name {
+            named_groups.insert(name.into(), value.clone());
+        }
+        capture_groups.push(value);
+    }
+
+    value!({
+        string: full_match,
+        captures: capture_groups,
+        named: named_groups,
+    })
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -104,28 +129,33 @@ impl Function for ReplaceWith {
         &[
             Example {
                 title: "double replacement",
-                source: r#"replace_with("foobar", r'o|a') -> |m| { m[0] + m[0] }"#,
+                source: r#"replace_with("foobar", r'o|a') -> |m| { m.string + m.string }"#,
                 result: Ok("foooobaar"),
             },
             Example {
                 title: "replace count",
-                source: r#"replace_with("foobar", r'o|a', count: 1) -> |m| { m[0] + m[0] }"#,
+                source: r#"replace_with("foobar", r'o|a', count: 1) -> |m| { m.string + m.string }"#,
                 result: Ok("fooobar"),
             },
             Example {
                 title: "replace with capture group",
-                source: r#"replace_with("foo123bar", r'foo(\d+)bar') -> |m| { x = m[1]; "x={{x}}" }"#,
+                source: r#"replace_with("foo123bar", r'foo(\d+)bar') -> |m| { x = m.captures[0]; "x={{x}}" }"#,
                 result: Ok(r#"x=123"#),
             },
             Example {
                 title: "process capture group",
-                source: r#"replace_with(s'Got message: {"msg": "b"}', r'message: (\{.*\})') -> |m| { to_string!(parse_json!(m[1]).msg) }"#,
+                source: r#"replace_with(s'Got message: {"msg": "b"}', r'message: (\{.*\})') -> |m| { to_string!(parse_json!(m.captures[0]).msg) }"#,
                 result: Ok("Got b"),
             },
             Example {
                 title: "Optional capture group",
-                source: r#"replace_with("foobar", r'bar( of gold)?') -> |m| { if m[1] == null { "baz" } else { "rich" } }"#,
+                source: r#"replace_with("foobar", r'bar( of gold)?') -> |m| { if m.captures[1] == null { "baz" } else { "rich" } }"#,
                 result: Ok("foobaz"),
+            },
+            Example {
+                title: "Named capture group",
+                source: r#"replace_with("foo123bar", r'foo(?P<num>\d+)bar') -> |m| { x = to_int!(m.named.num); to_string(x+ 1) }"#, //to_string(to_int!(m.named.num) + 1) }"#,
+                result: Ok("\"124\""),
             },
         ]
     }
@@ -154,21 +184,31 @@ impl Function for ReplaceWith {
     fn closure(&self) -> Option<closure::Definition> {
         use closure::{Definition, Input, Output, Variable, VariableKind};
 
+        let match_type: BTreeMap<Field, Kind> = BTreeMap::from([
+            ("string".into(), Kind::bytes()),
+            (
+                "captures".into(),
+                Kind::array(Collection::from_unknown(Kind::bytes().or_null())),
+            ),
+            (
+                "named".into(),
+                Kind::object(Collection::from_unknown(Kind::bytes().or_null())),
+            ),
+        ]);
+
         Some(Definition {
             inputs: vec![Input {
                 parameter_keyword: "value",
                 kind: Kind::bytes(),
                 variables: vec![
                     Variable {
-                        kind: VariableKind::Exact(Kind::array(Collection::from_parts(
-                                          BTreeMap::from([(0.into(), Kind::bytes())]),
-                                          Kind::bytes()))),
+                        kind: VariableKind::Exact(Kind::object(match_type)),
                     },
                 ],
                 output: Output::Kind(Kind::bytes()),
                 example: Example {
                     title: "replace with hash",
-                    source: r#"replace_with("received email from a@example.com", pattern: r'\w+@\w+\.\w+') -> |match| { sha2(match[0]) }"#,
+                    source: r#"replace_with("received email from a@example.com", pattern: r'\w+@\w+\.\w+') -> |match| { sha2(match.string) }"#,
                     result: Ok("received email from 896bdca840c9304a5d0bdbeacc4ef359e3093f80c9777c9967e31ba0ff99ed58"),
                 },
             }],
