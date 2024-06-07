e0b3477fdc35c7d025b0877453773252f04e2e80
Vladimir Zhuk <vladimir.zhuk@datadoghq.com>
2024-06-07 09:56:16 +0200

    fix: return back proptest_derive::Arbitrary for OwnedValuePath

--- src/path/owned.rs
+++ src/path/owned.rs
@@ -14,6 +14,7 @@ use crate::value::KeyString;
 /// A lookup path.
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
 #[serde(try_from = "String", into = "String")]
+#[cfg_attr(feature = "proptest", derive(proptest_derive::Arbitrary))]
 pub struct OwnedValuePath {
     pub segments: Vec<OwnedSegment>,
 }
