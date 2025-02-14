20d786db25a4629c4bb1fc6067d765e7e2493bf5
Ensar Sarajčić <dev@ensarsarajcic.com>
2025-02-14 16:02:49 +0100

    Update ctx type

--- src/stdlib/shannon_entropy.rs
+++ src/stdlib/shannon_entropy.rs
@@ -141,7 +141,7 @@ impl Function for ShannonEntropy {
     fn compile(
         &self,
         state: &state::TypeState,
-        _ctx: &mut FunctionCompileContext,
+        _ctx: &mut CompileContext,
         arguments: ArgumentList,
     ) -> Compiled {
         let value = arguments.required("value");
