fad045348555c9c1cbcce7ad2cd97bc438ca7e4f
Jakub Onderka <jakub.onderka@gmail.com>
2025-02-19 11:53:22 +0100

    performance(stdlib): Avoid allocation for some VRL functions

--- src/stdlib/string_utils.rs
+++ src/stdlib/string_utils.rs
@@ -1,10 +1,11 @@
 use crate::prelude::{Value, ValueError, VrlValueConvert};
+use std::borrow::Cow;
 
-pub(crate) fn convert_to_string(value: &Value, to_lowercase: bool) -> Result<String, ValueError> {
+pub(crate) fn convert_to_string(value: &Value, to_lowercase: bool) -> Result<Cow<str>, ValueError> {
     let string = value.try_bytes_utf8_lossy()?;
     Ok(if to_lowercase {
-        string.to_lowercase()
+        Cow::Owned(string.to_lowercase())
     } else {
-        string.to_string()
+        string
     })
 }
