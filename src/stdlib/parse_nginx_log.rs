5a210ade8d086cabc207c8da51ad5640756072f3
Jakub Onderka <jakub.onderka@gmail.com>
2025-02-19 19:08:16 +0100

    fix(stdlib): Parsing nginx error log message with comma

--- src/stdlib/parse_nginx_log.rs
+++ src/stdlib/parse_nginx_log.rs
@@ -668,5 +668,26 @@ mod tests {
             }),
             tdef: TypeDef::object(kind_error()).fallible(),
         }
+
+        error_message_with_comma {
+            args: func_args![
+                value: r#"2022/05/30 20:56:22 [info] 3134#0: *99247 epoll_wait() reported that client prematurely closed connection, so upstream connection is closed too (104: Connection reset by peer) while reading upstream, client: 10.244.0.0, server: example.org, request: "GET / HTTP/1.1", upstream: "fastcgi://unix:/run/php-fpm/php8.3-fpm.sock:", host: "example:8080""#,
+                format: "error"
+            ],
+            want: Ok(btreemap! {
+                "timestamp" => Value::Timestamp(DateTime::parse_from_rfc3339("2022-05-30T20:56:22Z").unwrap().into()),
+                "severity" => "info",
+                "pid" => 3134,
+                "tid" => 0,
+                "cid" => 99_247,
+                "message" => "epoll_wait() reported that client prematurely closed connection, so upstream connection is closed too (104: Connection reset by peer) while reading upstream",
+                "client" => "10.244.0.0",
+                "server" => "example.org",
+                "request" => "GET / HTTP/1.1",
+                "host" => "example:8080",
+                "upstream" => "fastcgi://unix:/run/php-fpm/php8.3-fpm.sock:",
+            }),
+            tdef: TypeDef::object(kind_error()).fallible(),
+        }
     ];
 }
