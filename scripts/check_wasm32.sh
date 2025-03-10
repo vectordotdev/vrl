383a59f581000187b3ee383fe85c0850e975e983
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2025-03-10 17:07:01 -0400

    we can remove the verbose check

--- scripts/check_wasm32.sh
+++ scripts/check_wasm32.sh
@@ -3,4 +3,4 @@ echo "Add wasm32-unknown-unknown target"
 rustup target add wasm32-unknown-unknown
 
 echo "Run check"
-cargo check -v --target wasm32-unknown-unknown --no-default-features --features stdlib
+cargo check --target wasm32-unknown-unknown --no-default-features --features stdlib
