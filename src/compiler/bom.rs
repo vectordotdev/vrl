4557a707b16e12b7aba39038205cdf6c37b88049
Thomas Queiroz <thomasqueirozb@gmail.com>
2025-04-07 18:35:20 -0400

    Use BOM_MARKER/BOM_MARKER_BYTES instead of magic numbers

--- src/compiler/bom.rs
+++ src/compiler/bom.rs
@@ -7,25 +7,28 @@ pub trait StripBomFromUTF8 {
 }
 
 // \u{feff} and [0xef, 0xbb, 0xbf] are the same
+static BOM_MARKER_BYTES: &[u8] = &[0xef, 0xbb, 0xbf];
+static BOM_MARKER: char = '\u{feff}';
+
 impl StripBomFromUTF8 for &str {
     fn strip_bom(self) -> Self {
-        self.trim_start_matches("\u{feff}")
+        self.trim_start_matches(BOM_MARKER)
     }
 }
 
 impl StripBomFromUTF8 for &[u8] {
     fn strip_bom(self) -> Self {
-        self.strip_prefix(&[0xef, 0xbb, 0xbf]).unwrap_or(self)
+        self.strip_prefix(BOM_MARKER_BYTES).unwrap_or(self)
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::StripBomFromUTF8;
+    use super::*;
 
     #[test]
     fn test_strip_bom_str_from_bytes() {
-        let raw = &[0xef, 0xbb, 0xbf, 0x7b, 0x7d]; // BOM{}
+        let raw = &[BOM_MARKER_BYTES, &[0x7b, 0x7d]].concat(); // BOM{}
         let raw: &str = std::str::from_utf8(raw).unwrap();
         assert_eq!(raw.len(), 5);
         assert_eq!(raw, "\u{feff}{}");
@@ -37,8 +40,9 @@ mod test {
 
     #[test]
     fn test_strip_bom_str_with_utf8_escape() {
-        let raw: &str = "\u{feff}{}"; // BOM{}
-                                      // Should be the exact same as the test from raw bytes
+        let raw = format!("{}{{}}", BOM_MARKER); // BOM{}
+                                                 // Should be the exact same as the test from raw bytes
+        let raw: &str = raw.as_str();
         assert_eq!(raw.len(), 5);
 
         let stripped = raw.strip_bom();
@@ -48,7 +52,7 @@ mod test {
 
     #[test]
     fn test_strip_bom_u8_slice() {
-        let raw: &[u8] = &[0xef, 0xbb, 0xbf, 0x7b, 0x7d]; // BOM{}
+        let raw = &[BOM_MARKER_BYTES, &[0x7b, 0x7d]].concat(); // BOM{}
         assert_eq!(raw.len(), 5);
 
         let stripped = raw.strip_bom();
