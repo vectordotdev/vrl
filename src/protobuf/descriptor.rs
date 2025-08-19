77b47df5968f38204d8e3e4a58ce6da4ffe12a34
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2025-08-19 13:36:20 -0400

    chore(protobuf): add 'get_message_descriptor_from_bytes' util (#1477)
    * chore(protobuf): add 'get_message_descriptor_from_bytes' util
    
    wip
    
    * cargo update -p slab

--- src/protobuf/descriptor.rs
+++ src/protobuf/descriptor.rs
@@ -4,14 +4,34 @@ use std::path::Path;
 pub fn get_message_descriptor(
     descriptor_set_path: &Path,
     message_type: &str,
-) -> std::result::Result<MessageDescriptor, String> {
-    let b = std::fs::read(descriptor_set_path).map_err(|e| {
-        format!("Failed to open protobuf desc file '{descriptor_set_path:?}': {e}",)
-    })?;
-    let pool = DescriptorPool::decode(b.as_slice()).map_err(|e| {
+) -> Result<MessageDescriptor, String> {
+    let bytes = std::fs::read(descriptor_set_path)
+        .map_err(|e| format!("Failed to open protobuf desc file '{descriptor_set_path:?}': {e}"))?;
+    let pool = DescriptorPool::decode(bytes.as_slice()).map_err(|e| {
         format!("Failed to parse protobuf desc file '{descriptor_set_path:?}': {e}")
     })?;
-    pool.get_message_by_name(message_type).ok_or_else(|| {
-        format!("The message type '{message_type}' could not be found in '{descriptor_set_path:?}'")
-    })
+    resolve_message_descriptor(
+        pool,
+        message_type,
+        &format!("'{}'", descriptor_set_path.display()),
+    )
+}
+
+pub fn get_message_descriptor_from_bytes(
+    descriptor_bytes: &[u8],
+    message_type: &str,
+) -> Result<MessageDescriptor, String> {
+    let pool = DescriptorPool::decode(descriptor_bytes)
+        .map_err(|e| format!("Failed to parse protobuf descriptor bytes: {e}"))?;
+    resolve_message_descriptor(pool, message_type, "the provided descriptor bytes")
+}
+
+fn resolve_message_descriptor(
+    descriptor_pool: DescriptorPool,
+    message_type: &str,
+    context: &str,
+) -> Result<MessageDescriptor, String> {
+    descriptor_pool
+        .get_message_by_name(message_type)
+        .ok_or_else(|| format!("The message type '{message_type}' could not be found in {context}"))
 }
