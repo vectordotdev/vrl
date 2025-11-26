9257e639a98d7f85504892c697220cd040e464b1
Thomas <thomas.schneider@datadoghq.com>
2025-11-26 16:43:44 -0500

    Sync and mock now function

--- src/stdlib/now.rs
+++ src/stdlib/now.rs
@@ -9,12 +9,29 @@ impl Function for Now {
         "now"
     }
 
+    #[cfg(not(feature = "test"))]
     fn examples(&self) -> &'static [Example] {
         &[example! {
             title: "now",
             source: r#"now() != """#,
             result: Ok("true"),
         }]
+    }
+
+    #[cfg(feature = "test")]
+    fn examples(&self) -> &'static [Example] {
+        &[
+            example! {
+                title: "Generate a current timestamp",
+                source: r#"now()"#,
+                result: Ok("2012-03-04T12:34:56.789012345Z"),
+            },
+            example! {
+                title: "now",
+                source: r#"now() != """#,
+                result: Ok("true"),
+            },
+        ]
     }
 
     fn compile(
@@ -31,8 +48,19 @@ impl Function for Now {
 struct NowFn;
 
 impl FunctionExpression for NowFn {
+    #[cfg(not(feature = "test"))]
     fn resolve(&self, _: &mut Context) -> Resolved {
         Ok(Utc::now().into())
+    }
+
+    #[cfg(feature = "test")]
+    fn resolve(&self, _: &mut Context) -> Resolved {
+        use chrono::{NaiveDate, TimeZone};
+
+        let d = NaiveDate::from_ymd_opt(2012, 3, 4).unwrap();
+        let d = d.and_hms_nano_opt(12, 34, 56, 789_012_345).unwrap();
+
+        Ok(Utc.from_local_datetime(&d).unwrap().into())
     }
 
     fn type_def(&self, _: &state::TypeState) -> TypeDef {
