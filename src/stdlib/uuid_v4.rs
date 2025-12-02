52672fb8d405ebdf60ce8537a22deaca375fbb8b
Thomas <thomas.schneider@datadoghq.com>
2025-12-02 17:36:35 -0500

    chore(tests): mock implementations for examples and tests (#1574)
    * Sync and mock now function
    
    * Mock UUIDv4 in tests
    
    * Mock UUIDv7 in tests
    
    * Add and use __mock_return_values_for_tests
    
    * Run mocked tests
    
    * Don't include empty example test
    
    * Add set -e to scripts/vrl_tests.sh to fail if command fails
    
    * Sync random_bytes with mocking

--- src/stdlib/uuid_v4.rs
+++ src/stdlib/uuid_v4.rs
@@ -1,6 +1,7 @@
 use crate::compiler::prelude::*;
 use bytes::Bytes;
 
+#[cfg_attr(feature = "__mock_return_values_for_tests", allow(dead_code))]
 fn uuid_v4() -> Value {
     let mut buf = [0; 36];
     let uuid = uuid::Uuid::new_v4().hyphenated().encode_lower(&mut buf);
@@ -15,11 +16,21 @@ impl Function for UuidV4 {
         "uuid_v4"
     }
 
+    #[cfg(not(feature = "__mock_return_values_for_tests"))]
     fn examples(&self) -> &'static [Example] {
         &[example! {
-            title: "generate UUID v4",
+            title: "Create a UUIDv4",
             source: r#"uuid_v4() != """#,
             result: Ok("true"),
+        }]
+    }
+
+    #[cfg(feature = "__mock_return_values_for_tests")]
+    fn examples(&self) -> &'static [Example] {
+        &[example! {
+            title: "Create a UUIDv4",
+            source: r#"uuid_v4()"#,
+            result: Ok("1d262f4f-199b-458d-879f-05fd0a5f0683"),
         }]
     }
 
@@ -37,8 +48,14 @@ impl Function for UuidV4 {
 struct UuidV4Fn;
 
 impl FunctionExpression for UuidV4Fn {
+    #[cfg(not(feature = "__mock_return_values_for_tests"))]
     fn resolve(&self, _: &mut Context) -> Resolved {
         Ok(uuid_v4())
+    }
+
+    #[cfg(feature = "__mock_return_values_for_tests")]
+    fn resolve(&self, _: &mut Context) -> Resolved {
+        Ok("1d262f4f-199b-458d-879f-05fd0a5f0683".into())
     }
 
     fn type_def(&self, _: &TypeState) -> TypeDef {
