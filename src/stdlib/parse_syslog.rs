193002c6eb89d39c3cd7a35e6b3a91a75a030b4a
Thomas <thomas.schneider@datadoghq.com>
2025-12-04 16:42:46 -0500

    Sync parse_syslog

--- src/stdlib/parse_syslog.rs
+++ src/stdlib/parse_syslog.rs
@@ -36,7 +36,7 @@ impl Function for ParseSyslog {
 
     fn examples(&self) -> &'static [Example] {
         &[example! {
-            title: "parse syslog",
+            title: "Parse Syslog log (5424)",
             source: r#"parse_syslog!(s'<13>1 2020-03-13T20:45:38.119Z dynamicwireless.name non 2426 ID931 [exampleSDID@32473 iut="3" eventSource= "Application" eventID="1011"] Try to override the THX port, maybe it will reboot the neural interface!')"#,
             result: Ok(indoc! {r#"{
                 "appname": "non",
