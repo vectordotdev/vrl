05607e93650805ae21c6a1a3d8eb572677ba482e
Thomas <thomas.schneider@datadoghq.com>
2026-02-20 17:15:57 -0500

    Remove unused import

--- src/stdlib/encode_proto.rs
+++ src/stdlib/encode_proto.rs
@@ -3,7 +3,6 @@ use crate::compiler::prelude::*;
 use crate::protobuf::descriptor::get_message_descriptor;
 use crate::protobuf::encode::encode_proto;
 use prost_reflect::MessageDescriptor;
-use std::env;
 use std::ffi::OsString;
 use std::path::Path;
 use std::path::PathBuf;
