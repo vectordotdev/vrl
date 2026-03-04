e37d0ff85e98ec5d1e9d740c21cb779fc520fccc
Thomas <thomas.schneider@datadoghq.com>
2026-03-04 11:37:14 -0500

    Address PR review

--- lib/docs/src/main.rs
+++ lib/docs/src/main.rs
@@ -1,6 +1,8 @@
+use std::process::ExitCode;
+
 use clap::Parser;
 use vrl::docs::{Opts, cmd::docs};
 
-fn main() {
-    std::process::exit(docs(&Opts::parse(), &vrl::stdlib::all()));
+fn main() -> ExitCode {
+    ExitCode::from(docs(&Opts::parse(), &vrl::stdlib::all()) as u8)
 }
