0bea0bf7a0ff3efebbcbb9fb0c6bda0f22cd7ad2
Thomas <thomas.schneider@datadoghq.com>
2026-03-20 14:41:46 -0400

    Use Gen::from_size_and_seed with u64::arbitrary(g)

--- src/value/value/arbitrary.rs
+++ src/value/value/arbitrary.rs
@@ -71,7 +71,7 @@ impl Arbitrary for Value {
             3 => Self::Boolean(bool::arbitrary(g)),
             4 => Self::Timestamp(datetime(g)),
             5 => {
-                let mut generator = Gen::new(MAX_MAP_SIZE);
+                let mut generator = Gen::from_size_and_seed(MAX_MAP_SIZE, u64::arbitrary(g));
                 Self::Object(
                     // `Arbitrary` is not directly implemented for `KeyString` so have to convert.
                     Vec::<(String, Self)>::arbitrary(&mut generator)
@@ -81,7 +81,7 @@ impl Arbitrary for Value {
                 )
             }
             6 => {
-                let mut generator = Gen::new(MAX_ARRAY_SIZE);
+                let mut generator = Gen::from_size_and_seed(MAX_ARRAY_SIZE, u64::arbitrary(g));
                 Self::Array(Vec::arbitrary(&mut generator))
             }
             7 => Self::Null,
