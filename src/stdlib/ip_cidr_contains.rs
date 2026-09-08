575ef61aade8fdff6441d71d2ecc226402fb76fe
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2026-09-08 14:26:27 -0400

    fix(stdlib): align ip_cidr_contains argument order

--- src/stdlib/ip_cidr_contains.rs
+++ src/stdlib/ip_cidr_contains.rs
@@ -173,12 +173,14 @@ struct IpCidrContainsFn {
 
 impl FunctionExpression for IpCidrContainsFn {
     fn resolve(&self, ctx: &mut Context) -> Resolved {
-        let value = self.value.resolve(ctx)?;
-
         match &self.cidr {
-            ConstOrExpr::Const(cidr_vec) => ip_cidr_contains_constant(&value, cidr_vec),
+            ConstOrExpr::Const(cidr_vec) => {
+                let value = self.value.resolve(ctx)?;
+                ip_cidr_contains_constant(&value, cidr_vec)
+            }
             ConstOrExpr::Expr(expr) => {
                 let cidr = expr.resolve(ctx)?;
+                let value = self.value.resolve(ctx)?;
                 ip_cidr_contains(&value, &cidr)
             }
         }
